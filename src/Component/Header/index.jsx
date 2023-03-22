713a3a3d9988190fcc7d0815714dd3f651afcdef
evans.moris21 <evans.moris21@gmail.com>
2023-03-22 11:38:22 +0700

    first commit for create header component

---
+++ src/Component/Header/index.jsx
@@ -0,0 +1,53 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function Header() {
+  return (
+    <>
+      {['sm'].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#">Bootcamp</Navbar.Brand>
+            <Navbar.Offcanvas
+            >
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">About</Nav.Link>
+                  <NavDropdown
+                    title="Class"
+                  >
+                    <NavDropdown.Item href="#action3">FrontEnd</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      BackEnd
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Fullstack
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default Header;
